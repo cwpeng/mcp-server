61083b81e43c6aad897da982e20e128f1f553f6a
cwpeng <padalab@gmail.com>
2025-12-28 08:56:12 +0800

    Build Pie Chart Tool

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
+import matplotlib.pyplot as plt
+import io
 from fastmcp import FastMCP
+from fastmcp.utilities.types import Image
 app=FastMCP("My MCP Server")
 # 提供一個加法的工具
 @app.tool
 def add(n1:int, n2:int)->int:
   """Add Two Numbers"""
   return n1+n2
+# 提供一個繪製圓餅圖的工具
+@app.tool
+def draw_pie_chart(numbers:list[int])->Image:
+  """Draw Pie Chart"""
+  plt.pie(numbers)
+  buffer=io.BytesIO()
+  plt.savefig(buffer, format="png")
+  return Image(data=buffer.getvalue(), format="png")
